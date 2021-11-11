cf1aa2efdf2993e5c3bf098e9001ad62052e216c
Matheus <morais.matheus.web@gmail.com>
2021-11-11 20:03:28 -0300

    Requesting and sending messages done

--- projeto5-Bate-Papo_uol/javascript.js
+++ projeto5-Bate-Papo_uol/javascript.js
@@ -3,12 +3,10 @@ function carregarPagina () {
     objNome = {name: nome};
     const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', objNome);
     promessa.then(verificarNome);
-
     return nome;
 }
 
 function verificarNome (resposta) {
-    console.log(resposta + "resposta de verificarNome");
     if (resposta.status == 200){
         return;
     } else if (resposta.status == 400) {
@@ -27,49 +25,46 @@ function buscarMensagens () {
     promessa.then(printarMensagens);
 }
 
-function printarMensagens(objMensagem) {
-    if (objMensagem.type === 'status') { 
-        main.innerHTML += `<div class="mensagem ${objMensagem.type}">
-                <span class="hora-mensagem">${objMensagem.time}</span>
-                <span class="nome-mensagem">${objMensagem.from} </span> 
-                <span class="conteudo-mensagem">${objMensagem.text}</span>
-            </div>`;
-    } else if (objMensagem.type === 'message') {
-        main.innerHTML += `<div class="mensagem ${objMensagem.type}">
-                <span class="hora-mensagem">${objMensagem.time}</span>
-                <span class="nome-mensagem">${objMensagem.from}</span>
-                <span> para <span>
-                <span class = "nome-mensagem>${objMensagem.to} </span>
-                <span class="conteudo-mensagem">${objMensagem.text}</span>
-            </div>`;
-    } else if (objMensagem.type === 'private_message') {
-        main.innerHTML += `<div class="mensagem ${objMensagem.type}">
-                <span class="hora-mensagem">${objMensagem.time}</span>
-                <span class="nome-mensagem">${objMensagem.from}</span>
-                <span> reservadamente para </span>
-                <span class = "nome-mensagem>${objMensagem.to} </span>
-                <span class="conteudo-mensagem">${objMensagem.text}</span>
-            </div>`;
-    }
-    const elementoQueQueroQueApareca = main.lastChild;
-    elementoQueQueroQueApareca.scrollIntoView;
-    
+function printarMensagens(objTodasMensagens) {
+    const arrayMensagensServidor = objTodasMensagens.data;
 
+    for (let i=0;i<arrayMensagensServidor.length;i++){
+        const objMensagem = arrayMensagensServidor[i];
+        if (objMensagem.type === 'status') { 
+            main.innerHTML += `<div class="mensagem ${objMensagem.type}">
+                                    <span> 
+                                        <span class="hora-mensagem">${objMensagem.time}</span> <strong>${objMensagem.from}</strong> ${objMensagem.text}
+                                    </span>
+                                </div>`;
+
+        } else if (objMensagem.type === 'message') {
+            main.innerHTML += `<div class="mensagem ${objMensagem.type}">
+                                    <span> 
+                                        <span class="hora-mensagem">${objMensagem.time}</span> <strong>${objMensagem.from}</strong> para <strong>${objMensagem.to}</strong> ${objMensagem.text}
+                                    </span>
+                                </div>`;
+            
+        } else if (objMensagem.type === 'private_message') {
+            main.innerHTML += `<div class="mensagem ${objMensagem.type}">
+                                    <span> 
+                                        <span class="hora-mensagem">${objMensagem.time}</span> <strong>${objMensagem.from}</strong> reservadamente para <strong>${objMensagem.to}</strong> ${objMensagem.text}
+                                    </span>
+                                </div>`;
+            
+        }
+    }
+    main.lastChild.scrollIntoView();
 }
 
 function tentarEnviarMensagem () {
-    const mensagemEnviada = {from: nome, to:'', text:'', type:'message'};
+    const mensagemEnviada = {from: nome, to:'Todos', text:'', type:'message'};
     mensagemEnviada.text = document.querySelector("input").value;
     const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', mensagemEnviada);
-    promessa.then(enviarMensagem);
+    promessa.catch(recarregarPagina);
 }
 
-function enviarMensagem (resposta) {
-    if (resposta.status == 200){
-        console.log('mensagem enviada com sucesso?');
-    } else if (resposta.status == 400) {
-        console.log('mensagem nao enviada com sucesso meu chapa');
-    }
+function recarregarPagina() {
+    window.location.reload();
 }
 
 let objNome = {};
@@ -79,4 +74,5 @@ const main = document.querySelector('main');
 let nome = carregarPagina();
 setInterval(verSeEstaNaSala, 5000);
 setInterval(buscarMensagens, 3000);
+
 
